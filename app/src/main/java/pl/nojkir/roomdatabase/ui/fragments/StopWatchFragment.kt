3fcbe39a05bec1e42c7d73997f31017456411147
nojkir <neukirch.patryk@gmail.com>
2020-08-17 00:30:58 +0200

    FormattedText method to StopWatch Fragment, and delete chronometer methods

--- app/src/main/java/pl/nojkir/roomdatabase/ui/fragments/StopWatchFragment.kt
+++ app/src/main/java/pl/nojkir/roomdatabase/ui/fragments/StopWatchFragment.kt
@@ -15,19 +15,24 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_stopwatch.*
 import pl.nojkir.roomdatabase.R
 import pl.nojkir.roomdatabase.ui.StopWatchViewModel
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class StopWatchFragment : Fragment(R.layout.fragment_stopwatch) {
     private lateinit var stopwatch: Chronometer
-    private lateinit var start : ImageButton
-    private lateinit var pause : ImageButton
-    private lateinit var reset : ImageButton
+    private lateinit var start: ImageButton
+    private lateinit var pause: ImageButton
+    private lateinit var reset: ImageButton
 
 
     private val viewModel: StopWatchViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-         val view = inflater.inflate(R.layout.fragment_stopwatch, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_stopwatch, container, false)
         stopwatch = view.findViewById(R.id.chronometer)
 
         start = view.findViewById(R.id.button_start)
@@ -42,88 +47,29 @@ class StopWatchFragment : Fragment(R.layout.fragment_stopwatch) {
         super.onViewCreated(view, savedInstanceState)
 
 
-        if (viewModel.startTime == null){
-            viewModel.startTime = 0
-
-        }else{
-            if (!viewModel.isVisible){
-                stopwatch.visibility = View.GONE
-                info_TV.text = viewModel.infoText
-                info_TV.visibility = View.VISIBLE
-            }
-            stopwatch.base = viewModel.startTime!!
-            stopwatch.start()
-        }
-
-
-        start.setOnClickListener { startChronometer() }
-        pause.setOnClickListener { pauseChronometer() }
-        reset.setOnClickListener { resetChronometer() }
-
     }
 
-
-
-        fun startChronometer(){
-                if (!viewModel.isRunning) {
-                    stopwatch.apply {
-                        base = SystemClock.elapsedRealtime() - viewModel.startTime!!
-                        stopwatch.visibility = View.VISIBLE
-                        start()
-                    }
-                   viewModel.apply {
-                       startTime = stopwatch.base
-                       isRunning = true
-                       isVisible = true
-                   }
-                    info_TV.visibility = View.GONE
-                }
-
+    fun getFormattedStopWatchTime(ms: Long, includeMillis : Boolean = false) :String{
+        var milliseconds = ms
+        val hours = TimeUnit.MILLISECONDS.toHours(milliseconds)
+        milliseconds -= TimeUnit.HOURS.toMillis(hours)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds)
+        milliseconds -= TimeUnit.MINUTES.toMillis(minutes)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)
+        if (!includeMillis){
+            return "${if (hours < 10) "0" else ""}$hours:" +
+                    "${if (minutes < 10) "0" else ""}$minutes:" +
+                    "${if (seconds < 10) "0" else ""}$seconds"
         }
+        milliseconds -= TimeUnit.SECONDS.toMillis(seconds)
+        milliseconds /= 10
 
-        fun pauseChronometer () {
-            if (viewModel.isRunning) {
-                stopwatch.apply {
-                    stop()
-                    visibility = View.GONE
-                }
-                viewModel.apply {
-                    isRunning = false
-                    startTime = SystemClock.elapsedRealtime() - stopwatch.base
-                    isVisible = false
-                    infoText = "Tap start to resume"
-                }
-                info_TV.apply {
-                    text = viewModel.infoText + " " + viewModel.startTime!!/1000 + " seconds"
-                    visibility = View.VISIBLE
-                }
-
-            }
-        }
-
-            fun resetChronometer(){
-                stopwatch.apply {
-                    base = SystemClock.elapsedRealtime()
-                    stop()
-                    visibility = View.GONE
-                }
-
-                viewModel.apply {
-                    startTime = 0
-                    isVisible = false
-                    isRunning = false
-                    infoText = "Tap start to start"
-                }
-
-
-                info_TV.apply {
-                    text = viewModel.infoText
-                    visibility = View.VISIBLE
-                }
-
-            }
-
-            }
+        return "${if (hours < 10) "0" else ""}$hours:" +
+                "${if (minutes < 10) "0" else ""}$minutes:" +
+                "${if (seconds < 10) "0" else ""}$seconds:" +
+                "${if (milliseconds < 10) "0" else ""}$milliseconds"
+    }
+}
 
 
 
