84c37344b43d0918bbaaf451728275cb8efc2a0e
nojkir <neukirch.patryk@gmail.com>
2020-09-01 21:47:33 +0200

    Created TrainingDao.kt

--- app/src/main/java/pl/nojkir/roomdatabase/data/daos/TrainingDao.kt
+++ app/src/main/java/pl/nojkir/roomdatabase/data/daos/TrainingDao.kt
@@ -1,8 +1,21 @@
 package pl.nojkir.roomdatabase.data.daos
 
-import androidx.room.Dao
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import pl.nojkir.roomdatabase.data.db.entities.Training
 
 
 @Dao
 interface TrainingDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(vararg training: Training)
+
+    @Delete
+    suspend fun delete(vararg training: Training)
+
+    @Query ("SELECT * FROM trainings_table")
+    fun getAllTrainings(): LiveData<List<Training>>
+
+
 }
